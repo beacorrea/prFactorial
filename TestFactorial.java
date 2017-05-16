abf41395be63246ea8723ff0d279d74c56380195
beacorrea <beacorrea@uma.es>
2017-05-16 09:55:45 +0200

    Version final

--- TestFactorial.java
+++ TestFactorial.java
@@ -2,10 +2,24 @@ package prFactorial;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class TestFactorial {
-
+	
+	@Parameters
+	public static Collection<Integer[]> valores(){
+		Integer[][] val = {{0,1},{1,1},{2,2},{3,6},{4,24},{5,120}};
+		
+		return Arrays.asList(val);
+	}
+	
 	@Test
 	public void factorial0es1() {
 		assertEquals(new Integer(1),new Integer(Factorial.calcular(0)));
@@ -29,5 +43,16 @@ public class TestFactorial {
 	@Test
 	public void factorial5es120() {
 		assertEquals(new Integer(120),new Integer(Factorial.calcular(5)));
+	}
+	private int entrada,salida;
+	
+	public TestFactorial(int c, int s){
+		entrada = c;
+		salida = s;
+	}
+	
+	@Test
+	public void factorialEntradaSalida(){
+		assertEquals(new Integer(salida),new Integer(Factorial.calcular(entrada)));
 	}
 }
